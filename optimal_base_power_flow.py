85cdc83209c2e82192b3b96facc0c06ae4b9502b
Jhon A Torres <131829083+JhonTorres13@users.noreply.github.com>
2025-10-14 02:05:55 -0500

    Update optimal_base_power_flow.py

--- optimal_base_power_flow.py
+++ optimal_base_power_flow.py
@@ -20,6 +20,7 @@ n=datos_n.shape[0]     # lineas
 # #pasar datos a p.u.
 nodos_l=np.array(datos_l[['Nodo i', 'Nodo j']]-1).reshape(l,2)
 rx=np.array(datos_l[['resistencia [ohmio]', 'reactancia [ohmio]']]/z_base).reshape(l,2)
+y = 1/(rx[:,0]+1j*rx[:,1])  # vector de admitancias
 nodos_n=np.array(datos_n['Nodo']-1).reshape(n,1)
 pq_activa=np.array((datos_n[['Pload [Kw]', 'Qload  [Kvar]']]*1000)/p_base).reshape(n,2)
 
@@ -44,22 +45,32 @@ wl= cvx.Variable((l),complex=True)   #variable compleja
 # === Restricciones ===
 Funcion_objetivo= cvx.sum(sk-(pq_activa[:,0]+ 1j*pq_activa[:,1]))
 res=[]
-res+=[u[0]==1]
-res+=[sk-(pq_activa[:,0]+ 1j*pq_activa[:,1])  ==Apositiva@Skm+Anegativa@Smk]   #balance de potencia
+res+=[u[0]==1]                                                                #nodo slack
+res+=[sk-(pq_activa[:,0]+ 1j*pq_activa[:,1]) ==Apositiva@Skm+Anegativa@Smk]   #balance de potencia
 res+=[u>=0.8]                                                                  #limite inferior de tension
 res+=[u<=1.1]                                                                  #limite superior de tension
 
-for i in range(1,n):
-    res += [cvx.abs(sk[i]) <= 0]                                               # solo un nodo de holgura
+res += [cvx.abs(sk[1:n]) <= 0]                                               # solo un nodo de holgura
+
+res += [Skm == cvx.multiply(np.conj(y),((Apositiva.T @ u) - wl))]            #flujo de potencia km
+res += [Smk == cvx.multiply(np.conj(y),((Anegativa.T @ u) - cvx.conj(wl)))]  #flujo de potencia mk
 
 for i in range(l):
-    admitancia=1/(rx[i,0]+rx[i,1]*1j)
-    res+=[Skm[i]==admitancia.conjugate()*(u[int(nodos_l[i,0])]-wl[i])]
-    res+=[Smk[i]==admitancia.conjugate()*(u[int(nodos_l[i,1])]-cvx.conj(wl[i]))]
     res+=[cvx.SOC(u[int(nodos_l[i,0])]+u[int(nodos_l[i,1])],cvx.vstack([2 * wl[i],u[int(nodos_l[i,0])]-u[int(nodos_l[i,1])]]))]
+
+
+# === Restricciones de los capacitores ===
+res += [skc == z @ capacitores]
+res += [cvx.sum(z,axis=0)  <= 1]
+numero_capacitores_disponibles=0      #Numero de capacitores disponibles
+res += [cvx.sum(z) <= numero_capacitores_disponibles]
+
 
 # === Función objetivo y resolución ===
 obj = cvx.Minimize(cvx.real(Funcion_objetivo))
 Z= cvx.Problem(obj,res)
 Z.solve("MOSEK")
 print("Valor de las perdidas", obj.value*p_base, "KW", Z.status)
+
+
+
